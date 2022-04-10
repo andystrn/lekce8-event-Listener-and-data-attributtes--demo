05a85ec0e09dd5ea9a7f9771864431495dc791d3
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-10 10:29:26 +0200

    obtain value from data attributes

---
+++ main.js
@@ -0,0 +1,4 @@
+let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
+let druheOvoce = document.querySelector('#muj-druhy-div').dataset.ovoce;
+console.log(prvniOvoce);
+console.log(druheOvoce);
