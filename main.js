9878382194ed2c4dec77f6eabefa953e8d97ab12
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-10 10:33:27 +0200

    obtain data url attribute from bodz element

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ let prvniOvoce = document.querySelector('#muj-div').dataset.ovoce;
 let druheOvoce = document.querySelector('#muj-druhy-div').dataset.ovoce;
 console.log(prvniOvoce);
 console.log(druheOvoce);
+
+let dataUrl = document.querySelector('body').dataset.dataUrl;
+console.log(dataUrl);
